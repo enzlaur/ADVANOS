7d500889fab101d64d1e7639e615d8e73c9c14fa
Laurenz <laurenztino@yahoo.com>
2017-07-12 19:41:56 +0800

    testing monitors in java

---
+++ src/parallel/MonitorTest/test.java
@@ -0,0 +1,10 @@
+package parallel.MonitorTest;
+
+/**
+ * Created by laurenz on 12/07/2017.
+ */
+public class test {
+
+}
+
+
---
+++ src/parallel/test.java
@@ -0,0 +1,7 @@
+package parallel;
+
+/**
+ * Created by laurenz on 12/07/2017.
+ */
+public class test {
+}
